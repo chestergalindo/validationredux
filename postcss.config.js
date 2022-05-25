ffbcdf0dda236547a4be25b120c2c6b385346569
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-05-24 19:36:07 -0500

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require( "postcss-preset-env" )( { stage: 0 } ),
+    require( "autoprefixer" ),
+    require( "cssnano" )( {
+      "preset": [ "default", {
+        discardComments: {
+          removeAll: true,
+        },
+      } ],
+    } ),
+  ],
+};
