06a0dfff5d11620ffee43e62a6a44826a58be597
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-05-25 09:04:44 -0500

    feat: add first view

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,5 +1,35 @@
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 export const App = () => {
-  return <p> hello world </p>;
+  const { register, handleSubmit } = useForm();
+  const ListProduct: any = [];
+  const onSubmit = (data: any) => {
+    console.log(data);
+    ListProduct.push(data.searchWord);
+    console.log({ ListProduct });
+  };
+
+  return (
+    <body>
+      <div className="App">
+        <h1>Listado de Mercado</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            aria-label=""
+            {...register('searchWord')}
+            placeholder="Articulos de mercado"
+          />
+          <button type="submit">Buscar</button>
+        </form>
+        <div className="list">
+          <li>Milk</li>
+          <input type="text" placeholder="0" aria-label="5" />
+          <button>Agregar</button>
+          <button>eliminar</button>
+        </div>
+      </div>
+    </body>
+  );
 };
